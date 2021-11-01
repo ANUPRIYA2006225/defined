f22eea1c5d0f32837cb738ee7fe75e4a90f15b9e
ANUPRIYA2006225 <91813724+ANUPRIYA2006225@users.noreply.github.com>
2021-11-01 21:39:06 +0530

    Add files via upload

---
+++ javalink.js
@@ -0,0 +1,11 @@
+var x;
+console.log("Variable x is defined but value is not assigned to it");
+console.log("x = "+x);
+x=100;
+console.log("Assigned a value to variable x now");
+console.log("x = "+x);
+console.log("x is defined now")
+console.log("Didn't declare a variable y");
+console.log("y = ");
+console.log("As variable y is not declared so its the example of not defined variable")
+console.log(y);
